3e012db10be5c85649a486a6cee2adac721c626b
aivanns <slavchonok11@gmail.com>
2024-10-21 12:40:45 +0300

    experiment

--- src/shared/constants/messages.ts
+++ src/shared/constants/messages.ts
@@ -1,3 +1,4 @@
 export const NAME_REQUIRED = 'Please input name of key!';
 export const NAME_MAX_LENGTH = 'Name must be less than 15 characters';
 export const HASH_REQUIRED = 'Please select hash type!';
+export const BUTTON_TEXT_GENERATE = 'Key generation';
